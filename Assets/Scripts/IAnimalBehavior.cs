4a79a87602171b1145669a0b5421c7d1b9464d65
Bahadır Üçyıldız <bedo.baho32@yandex.com>
2020-08-28 01:39:54 +0300

    Initial commit

--- Assets/Scripts/IAnimalBehavior.cs
+++ Assets/Scripts/IAnimalBehavior.cs
@@ -1,11 +1,12 @@
-interface IAnimalBehavior{
+using UnityEngine;
+interface IAnimalBehavior {
     void GetHungry();
     void GetThirsty();
     void FindFood();
     void FindWater();
     void Eat();
     void Drink();  
-    void Move();  
+    void Move(Transform destination);  
     //void TakeDamage(float amount);
     void Reproduce();    
     void Die();
