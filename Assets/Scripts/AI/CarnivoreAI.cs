c97af2f0a35f9cf687ca15b8809f022b4212bfd5
mustafamese <mustafamese099@gmail.com>
2021-02-27 15:10:21 +0300

    -New AI class added.

--- Assets/Scripts/AI/CarnivoreAI.cs
+++ Assets/Scripts/AI/CarnivoreAI.cs
@@ -99,49 +99,6 @@ public class CarnivoreAI : MonoBehaviour
 
     #region Utilities
 
-    public void Move (Transform destination)
-    {
-
-        var direction = destination.position - _transform.position;
-        Debug.DrawRay (_transform.position, direction, Color.red);
-        _transform.rotation = Quaternion.Slerp (
-            _transform.rotation, Quaternion.LookRotation (destination.position), 20 * Time.deltaTime);
-
-        if (direction.magnitude > 2f)
-        {
-            _agent.SetDestination (destination.position);
-            var dist = Vector3.Distance (destination.position, _transform.position);
-            //_transform.Translate(direction.normalized * MovementSpeed * Time.deltaTime, Space.World);
-
-            if (dist < 5)
-            {
-                _agent.isStopped = true;
-                _stateManager.fsm.SetBool ("isWandering", false);
-                _stateManager.fsm.SetBool ("isIdling", true);
-            }
-        }
-    }
-    public void Move (Vector3 destination)
-    {
-
-        var direction = destination - _transform.position;
-        Debug.DrawRay (_transform.position, direction, Color.red);
-        _transform.rotation = Quaternion.Lerp (
-            _transform.rotation, Quaternion.LookRotation (destination), 20 * Time.deltaTime);
-
-        if (direction.magnitude > 2f)
-        {
-            _agent.SetDestination (destination);
-            var dist = Vector3.Distance (destination, _transform.position);
-
-            if (dist < 5)
-            {
-                _agent.isStopped = true;
-                _stateManager.fsm.SetBool ("isWandering", false);
-                _stateManager.fsm.SetBool ("isIdling", true);
-            }
-        }
-    }
     public static Vector3 RandomNavSphere (Vector3 origin, float dist, int layermask)
     {
         Vector3 randDirection = Random.insideUnitSphere * dist;
---
+++ Assets/Scripts/AI/CreatureAI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class CreatureAI : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Transform _transform;
+    private StateManager _stateManager;
+
+    private void Start() 
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _transform = GetComponent<Transform>();
+        _stateManager = GetComponent<StateManager>();
+    }
+
+    public void Move(Transform destination)
+    {
+
+        var direction = destination.position - _transform.position;
+        Debug.DrawRay(_transform.position, direction, Color.red);
+        _transform.rotation = Quaternion.Slerp(
+            _transform.rotation, Quaternion.LookRotation(destination.position), 20 * Time.deltaTime);
+
+        if (direction.magnitude > 2f)
+        {
+            _agent.SetDestination(destination.position);
+            var dist = Vector3.Distance(destination.position, _transform.position);
+            //_transform.Translate(direction.normalized * MovementSpeed * Time.deltaTime, Space.World);
+
+            if (dist < 5)
+            {
+                _agent.isStopped = true;
+                _stateManager.fsm.SetBool("isWandering", false);
+                _stateManager.fsm.SetBool("isIdling", true);
+            }
+        }
+    }
+    
+    public void Move(Vector3 destination)
+    {
+
+        var direction = destination - _transform.position;
+        Debug.DrawRay(_transform.position, direction, Color.red);
+        _transform.rotation = Quaternion.Lerp(
+            _transform.rotation, Quaternion.LookRotation(destination), 20 * Time.deltaTime);
+
+        if (direction.magnitude > 2f)
+        {
+            _agent.SetDestination(destination);
+            var dist = Vector3.Distance(destination, _transform.position);
+
+            if (dist < 5)
+            {
+                _agent.isStopped = true;
+                _stateManager.fsm.SetBool("isWandering", false);
+                _stateManager.fsm.SetBool("isIdling", true);
+            }
+        }
+    }
+
+}
