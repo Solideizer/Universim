50ee3944499f9251a780b0847e8d23e594ac00a1
Abdulkadir90 <abdulkadirsemiz@outlook.com>
2021-03-03 17:44:40 +0300

    Camera controller added
    Kamera x eksininde serbest şekilde, y ekseninde -90 ile 90 derece de kısıtlı şekilde dönebiliyor. "wasd" tuş setiyle hareket edebilirken, "cv" tuş setine yükselme ve alçalma atandı. ui'ın içine hareket tutorial'i eklendi.

---
+++ Assets/Scripts/Camera Controller/MoveCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] private int speed;
+    private Vector3 moveVector;
+    private Vector3 riseVector;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveVector = transform.right * x + transform.forward * z;
+        characterController.Move(moveVector * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            riseVector.y = speed * Time.deltaTime;
+            characterController.Move(riseVector);
+        }
+
+        if (Input.GetKey(KeyCode.V))
+        {
+            riseVector.y = speed * Time.deltaTime * -1;
+            characterController.Move(riseVector);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera Controller/RotateCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private Transform watcher;
+
+    [SerializeField] private float sensivity = 100F;
+    private float mouseY;
+    private float mouseX;
+    private float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        mouseY = sensivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+        mouseX = sensivity * Input.GetAxis("Mouse X") * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        watcher.Rotate(Vector3.up * mouseX);
+    }
+}
