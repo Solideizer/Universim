d6d8ebfc5bef732417153e1909cacacc4e1fdb3b
Bahadır Üçyıldız <bedo.baho32@yandex.com>
2021-06-17 22:13:49 +0300

    Statistics improvements

--- Assets/Scripts/StatisticsManager.cs
+++ Assets/Scripts/StatisticsManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,13 +12,17 @@ public class StatisticsManager : MonoBehaviour
     private int _currentNumOfChickens;
     public List<int> numberOfFoxes { get; } = new List<int> ();
     public List<int> numberOfChickens { get; } = new List<int> ();
+    public List<int> numberOfMaleFoxes { get; } = new List<int> ();
+    public List<int> numberOfFemaleFoxes { get; } = new List<int> ();
+    public List<int> numberOfMaleChickens { get; } = new List<int> ();
+    public List<int> numberOfFemaleChickens { get; } = new List<int> ();
     private void Start ()
     {
-        StartCoroutine (GetNumberOfFoxes ());
-        StartCoroutine (GetNumberOfChicken ());
+        StartCoroutine (GetFoxStats ());
+        StartCoroutine (GetChickenStats ());
     }
 
-    IEnumerator GetNumberOfFoxes ()
+    IEnumerator GetFoxStats ()
     {
         while (true)
         {
@@ -25,11 +30,31 @@ public class StatisticsManager : MonoBehaviour
             _currentNumOfFoxes = foxes.Length;
             numberOfFoxes.Add (_currentNumOfFoxes);
             Debug.Log ("fox " + _currentNumOfFoxes);
+
+            int _maleFoxAmount = 0;
+            int _femaleFoxAmount = 0;
+
+            for (int i = 0; i < foxes.Length; i++)
+            {
+                var id = foxes[i].GetComponent<AnimalAI> ().Identity;
+                if ((int) id.Sex == 0)
+                {
+                    _maleFoxAmount++;
+                }
+                else
+                {
+                    _femaleFoxAmount++;
+                }
+
+            }
+
+            numberOfMaleFoxes.Add (_maleFoxAmount);
+            numberOfFemaleFoxes.Add (_femaleFoxAmount);
             yield return new WaitForSeconds (interval);
         }
     }
 
-    IEnumerator GetNumberOfChicken ()
+    IEnumerator GetChickenStats ()
     {
         while (true)
         {
@@ -37,6 +62,25 @@ public class StatisticsManager : MonoBehaviour
             _currentNumOfChickens = chicken.Length;
             numberOfChickens.Add (_currentNumOfChickens);
             Debug.Log ("chicken " + _currentNumOfChickens);
+
+            int _maleChickenAmount = 0;
+            int _femaleChickenAmount = 0;
+
+            for (int i = 0; i < chicken.Length; i++)
+            {
+                var id = chicken[i].GetComponent<AnimalAI> ().Identity;
+                if ((int) id.Sex == 0)
+                {
+                    _maleChickenAmount++;
+                }
+                else
+                {
+                    _femaleChickenAmount++;
+                }
+            }
+            numberOfMaleChickens.Add (_maleChickenAmount);
+            numberOfFemaleChickens.Add (_femaleChickenAmount);
+
             yield return new WaitForSeconds (interval);
         }
 
